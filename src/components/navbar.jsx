a83bdcb6585ecc9591df64152c9b2d87a82b41b0
Nagaraju Kuna <nagarajukuna79@gmail.com>
2024-12-26 11:08:22 +0530

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,15 @@
 export function Navbar() {
   return (
-    <div className="container-fluid d-100 bg-dark text-white fw-bold  p-4 justify-content-center sticky-top">
-      <div className="fs-1">AV Pickles</div>
-      <div className="fs-4">Phone No: +91 9133697272</div>
+    <div className="container-fluid d-100 bg-dark text-white fw-bold justify-content-center sticky-top pb-1">
+      <div className="fs-1">
+        <span style={{ fontSize: "3rem" }}>
+          <span className="text-danger">A</span>
+          <span className="text-warning">V</span>{" "}
+        </span>
+        NON-VEG PICKLES
+      </div>
+      <div className="fs-3">Phone No: +91 9133697272</div>
+      <div className="fs-6">Courier Service available for all Places</div>
     </div>
   );
 }
--- src/components/pickles.jsx
+++ src/components/pickles.jsx
@@ -1,29 +1,31 @@
 import pickles from "../data/pickles.json";
 export function Pickles() {
+  const phoneNumber = "9133942256"; // Replace with your number
+  const message = encodeURIComponent("Hello, I need help!");
   return (
-    <div className="container-fluid d-flex flex-wrap">
+    <div className="container-fluid d-flex flex-wrap justify-content-center">
       {pickles.map((pickle, index) => (
         <div
           key={index}
-          className="card col-12 col-md-6 col-lg-4 p-2 m-1 bg-warning"
+          className="card col-12 col-md-6 col-lg-4 p-1 m-1 mb-4 bg-warning"
         >
+          <h3
+            className="card-title fw-bold pb-1 pt-1"
+            style={{
+              fontFamily: "Serif",
+            }}
+          >
+            {pickle.name}
+          </h3>
           <div className="card-img-top">
             <img
               src={`${process.env.PUBLIC_URL}${pickle.image}`}
               alt={pickle.name}
-              style={{ width: "100%", height: "250px", objectFit: "cover" }}
+              style={{ width: "100%", height: "300px", objectFit: "cover" }}
             ></img>
           </div>
-          <div className="card-body">
+          <div className="card-body mb-0 pb-0">
             <dl>
-              <div className="d-flex justify-content-between">
-                <dt>Name</dt>
-                <dd>{pickle.name}</dd>
-              </div>
-              <div className="d-flex justify-content-between">
-                <dt>Price</dt>
-                <dd>₹{pickle.price}</dd>
-              </div>
               <div className="d-flex justify-content-between">
                 <dt>Quantity</dt>
                 <dd>{pickle.quantity}</dd>
@@ -32,15 +34,49 @@ export function Pickles() {
                 <dt>Expiry</dt>
                 <dd>{pickle.expiry}</dd>
               </div>
+              <div className="d-flex justify-content-between mb-0 pb-0">
+                <dt>Price</dt>
+                <div className="d-flex flex-column align-items-end mb-0 pb-0">
+                  <dd className="text-danger text-decoration-line-through fs-5">
+                    ₹{pickle.oldprice}
+                  </dd>
+                  <dd className="text-success fs-2 fw-bold pb-0 mb-0">
+                    ₹{pickle.price}
+                  </dd>
+                  <dd
+                    className="text-dark p-0 m-0"
+                    style={{ fontSize: "0.7rem" }}
+                  >
+                    +Extra delivery charges
+                  </dd>
+                </div>
+              </div>
             </dl>
           </div>
           <div className="card-footer">
-            <button className="bg-success p-2 w-100 rounded-3">
-              Call to Above Number
-            </button>
+            <a
+              href={`https://wa.me/${phoneNumber}?text=${message}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                display: "inline-block",
+                padding: "10px 20px",
+                backgroundColor: "#25D366",
+                color: "#fff",
+                borderRadius: "5px",
+                textDecoration: "none",
+              }}
+            >
+              Chat on WhatsApp
+            </a>
           </div>
         </div>
       ))}
+      <img
+        src={`${process.env.PUBLIC_URL}${`/images/av-pickles-logo.png`}`}
+        alt="AV pickles logo"
+        style={{ width: "100%" }}
+      ></img>
     </div>
   );
 }
